2e278f811215f82738c65974c955006ad1e25de6
Léonard Jouve <leonard.jouve@gmail.com>
2026-04-27 15:34:35 +0200

    try fix ajax service

--- plugins/local/classes/external/get_groups.php
+++ plugins/local/classes/external/get_groups.php
@@ -27,11 +27,11 @@ namespace local_gitlab\external;
 
 defined('MOODLE_INTERNAL') || die();
 
-use external_api;
-use external_function_parameters;
-use external_value;
-use external_multiple_structure;
-use external_single_structure;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_value;
+use core_external\external_multiple_structure;
+use core_external\external_single_structure;
 
 class get_groups extends external_api {
     public static function execute_parameters() {
@@ -41,6 +41,8 @@ class get_groups extends external_api {
     }
 
     public static function execute($token) {
+        require_login();
+        
         global $DB;
 
         $params = self::validate_parameters(self::execute_parameters(), [
