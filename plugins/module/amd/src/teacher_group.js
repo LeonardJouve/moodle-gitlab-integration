5ae352fc2eef97be5ba71e323fa218e8002b8ae8
Léonard Jouve <leonard.jouve@gmail.com>
2026-06-15 14:50:12 +0200

    try fix amd

--- plugins/module/amd/src/teacher_group.js
+++ plugins/module/amd/src/teacher_group.js
@@ -1,20 +1,24 @@
-export const init = (id) => {
-    console.log('amddddddd');
+define([], function() {
+    return {
+        init: function(id) {
+            console.log('amddddddd');
 
-    ["ssh", "https", "checkout"].forEach((name) => {
-        const prefix = `#code-dropdown-${id}`;
-        const input = document.querySelector(`${prefix} .${name}-input`);
-        const button = document.querySelector(`${prefix} .${name}-button`);
-        const icon = document.querySelector(`${prefix} .${name}-button i`);
-        if (!input || !button || !icon) {
-            return;
-        }
+            ["ssh", "https", "checkout"].forEach((name) => {
+                const prefix = `#code-dropdown-${id}`;
+                const input = document.querySelector(`${prefix} .${name}-input`);
+                const button = document.querySelector(`${prefix} .${name}-button`);
+                const icon = document.querySelector(`${prefix} .${name}-button i`);
+                if (!input || !button || !icon) {
+                    return;
+                }
 
-        button.addEventListener("click", () => {
-            navigator.clipboard.writeText(input.value).then(() => {
-                icon.classList.replace("fa-copy", "fa-check");
-                setTimeout(() => icon.classList.replace("fa-check", "fa-copy"), 500);
+                button.addEventListener("click", () => {
+                    navigator.clipboard.writeText(input.value).then(() => {
+                        icon.classList.replace("fa-copy", "fa-check");
+                        setTimeout(() => icon.classList.replace("fa-check", "fa-copy"), 500);
+                    });
+                });
             });
-        });
-    });
-};
+        },
+    };
+});
