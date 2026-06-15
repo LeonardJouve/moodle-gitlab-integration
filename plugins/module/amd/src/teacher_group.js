b00385450fc30fb00c5661a394584350d0088814
Léonard Jouve <leonard.jouve@gmail.com>
2026-06-15 14:32:59 +0200

    handle copy urls

---
+++ plugins/module/amd/src/teacher_group.js
@@ -0,0 +1,20 @@
+export const init = (id) => {
+    console.log('amddddddd');
+
+    ["ssh", "https", "checkout"].forEach((name) => {
+        const prefix = `#code-dropdown-${id}`;
+        const input = document.querySelector(`${prefix} .${name}-input`);
+        const button = document.querySelector(`${prefix} .${name}-button`);
+        const icon = document.querySelector(`${prefix} .${name}-button i`);
+        if (!input || !button || !icon) {
+            return;
+        }
+
+        button.addEventListener("click", () => {
+            navigator.clipboard.writeText(input.value).then(() => {
+                icon.classList.replace("fa-copy", "fa-check");
+                setTimeout(() => icon.classList.replace("fa-check", "fa-copy"), 500);
+            });
+        });
+    });
+};
