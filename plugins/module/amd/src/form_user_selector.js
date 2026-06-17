5d20ca0dc3cb914cffa0e62b4de005358958ea10
Léonard Jouve <leonard.jouve@gmail.com>
2026-06-17 09:42:12 +0200

    fix amd imports

--- plugins/module/amd/src/form_user_selector.js
+++ plugins/module/amd/src/form_user_selector.js
@@ -1,4 +1,4 @@
-define(['jquery', 'core/ajax', 'core/str'], function($, Ajax, Str) {
+define(['jquery', 'core/ajax', 'core/templates', 'core/str'], function($, Ajax, Templates, Str) {
     return {
         processResults: function(selector, results) {
             if (!$.isArray(results)) {
