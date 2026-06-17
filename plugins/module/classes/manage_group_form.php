d6f632b885da670ea169d1266a0694eebbc5c67b
Léonard Jouve <leonard.jouve@gmail.com>
2026-06-17 10:16:03 +0200

    try default

--- plugins/module/classes/manage_group_form.php
+++ plugins/module/classes/manage_group_form.php
@@ -39,7 +39,10 @@ class mod_gitlab_manage_group_form extends moodleform {
             'groupid' => $this->_customdata->groupid,
         );
         $mform->addElement('autocomplete', 'userlist', 'TODO', array(), $options);
-        $mform->setDefault('userlist', [2]);
+        $mform->setDefault('userlist', [
+            'value' => 2,
+            'label' => 'test',
+        ]);
     }
 
     /**
