147183fc333a899422aedfda69dc69e59690a181
Léonard Jouve <leonard.jouve@gmail.com>
2026-06-19 09:06:18 +0200

    reviewer default

--- plugins/module/mod_form.php
+++ plugins/module/mod_form.php
@@ -147,6 +147,7 @@ class mod_gitlab_mod_form extends moodleform_mod {
             'courseid' => $this->get_course()->id,
         );
         $mform->addElement('autocomplete', 'reviewer', get_string('form_reviewer', 'mod_gitlab'), $existants, $options);
+        $mform->setDefault('reviewer', array_keys($existants));
         $mform->addElement(
             'static',
             'test',
