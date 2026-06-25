1d3624c183d34c74ee1740a0b6d87b7d9c7e5f54
Léonard Jouve <leonard.jouve@gmail.com>
2026-06-25 10:30:29 +0200

    improve layout

--- plugins/module/lang/en/gitlab.php
+++ plugins/module/lang/en/gitlab.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['button_create_repository'] = 'Create GitLab Repository';
 $string['calendar_due_date_event'] = 'Assignment due on {$a->due_date}: {$a->name}';
-$string['calendar_due_date_description'] = 'The assignment {$a->name} in the course {$a->course} is due soon.\n\nDue date : {$a->due_date}';
+$string['calendar_due_date_description'] = 'The assignment {$a->name} in the course {$a->course} is due soon.<br>Due date : {$a->due_date}';
 $string['form_due_date'] = 'Due date';
 $string['form_due_date_help'] = 'Practical work due date and time';
 $string['form_group_size'] = 'Group size';
