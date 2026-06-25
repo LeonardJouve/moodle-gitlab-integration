e345dd067fdbbe0242f382010c24d2e4b3ef1a77
Léonard Jouve <leonard.jouve@gmail.com>
2026-06-25 13:21:24 +0200

    upgrade version

--- plugins/module/version.php
+++ plugins/module/version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_gitlab';
 $plugin->release = '0.1.0';
-$plugin->version = 2026033001;
+$plugin->version = 2026033002;
 $plugin->requires = 2025100601;
 $plugin->maturity = MATURITY_ALPHA;
