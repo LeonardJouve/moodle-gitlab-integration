dc41168014a9faf53fe405443ba148c2d9d9a5a3
Léonard Jouve <leonard.jouve@gmail.com>
2026-06-26 12:30:03 +0200

    init customfield plugin

---
+++ plugins/customfield/db/install.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+function create_user_field() {
+    global $DB;
+
+    $category = $DB->insert_record('user_info_category', (object)[
+        'name' => 'GitLab',
+        'sortorder' => 1,
+    ]);
+
+    $DB->insert_record('user_info_field', (object)[
+        'shortname' => 'gitlab_username',
+        'name' => 'GitLab Username',
+        'datatype' => 'text',
+        'categoryid' => $category,
+        'visible' => 3, // user, teachers and admins
+    ]);
+}
+
+/**
+ * @package     customfield_gitlab
+ * @copyright   2026 Léonard Jouve leonard.jouve@gmail.com
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+function xmldb_customfield_gitlab_install() {
+    create_user_field();
+}
