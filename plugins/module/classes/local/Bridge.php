0ad7febc93807554f34e3d70b7fd3b74d966f961
Léonard Jouve <leonard.jouve@gmail.com>
2026-07-01 18:58:15 +0200

    create ci files

--- plugins/module/classes/local/Bridge.php
+++ plugins/module/classes/local/Bridge.php
@@ -61,7 +61,10 @@ class Bridge {
         
         // reviewers
         $this->resources->add_reviewers_as_maintainers($template->id, $moduleinstance->reviewer ?? []);
-        
+
+        // protected files ci
+        $this->resources->create_template_protected_files_ci($template->id);
+
         $secret = Webhook::generate_key();
         $webhook = $this->resources->create_template_webhook($template->id, $secret);
         $moduleinstance->webhook_secret = encryption::encrypt($secret);
