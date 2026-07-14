982d0782ceac30f67f6d5e5fe3893d028d2bffc9
Léonard Jouve <leonard.jouve@gmail.com>
2026-07-14 10:41:48 +0200

    unit tests

---
+++ plugins/customfield/tests/data_controller_test.php
@@ -0,0 +1,233 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for data_controller class
+ *
+ * @package     customfield_gitlab
+ * @copyright   2026 Léonard Jouve leonard.jouve@gmail.com
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace customfield_gitlab;
+
+use advanced_testcase;
+use core\encryption;
+
+/**
+ * Unit tests for data_controller class
+ *
+ * @package     customfield_gitlab
+ * @copyright   2026 Léonard Jouve leonard.jouve@gmail.com
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class data_controller_test extends advanced_testcase {
+
+    public function setUp(): void {
+        parent::setUp();
+        $this->resetAfterTest(true);
+    }
+
+    /**
+     * Test datafield method returns 'value'
+     */
+    public function test_datafield_returns_value(): void {
+        $fieldrecord = new \stdClass();
+        $fieldrecord->name = 'GitLab Token';
+        $fieldrecord->shortname = 'gitlab_token';
+        $fieldrecord->type = 'gitlab';
+        $fieldrecord->configdata = '{}';
+
+        $field = new field_controller(0, $fieldrecord);
+        $datarecord = new \stdClass();
+        $datarecord->fieldid = 0;
+
+        $controller = new data_controller(0, $datarecord, $field);
+
+        $this->assertEquals('value', $controller->datafield());
+    }
+
+    /**
+     * Test get_default_value returns empty string
+     */
+    public function test_get_default_value_returns_empty_string(): void {
+        $fieldrecord = new \stdClass();
+        $fieldrecord->name = 'GitLab Token';
+        $fieldrecord->shortname = 'gitlab_token';
+        $fieldrecord->type = 'gitlab';
+        $fieldrecord->configdata = '{}';
+
+        $field = new field_controller(0, $fieldrecord);
+        $datarecord = new \stdClass();
+        $datarecord->fieldid = 0;
+
+        $controller = new data_controller(0, $datarecord, $field);
+
+        $this->assertEquals('', $controller->get_default_value());
+    }
+
+    /**
+     * Test export_value returns null
+     */
+    public function test_export_value_returns_null(): void {
+        $fieldrecord = new \stdClass();
+        $fieldrecord->name = 'GitLab Token';
+        $fieldrecord->shortname = 'gitlab_token';
+        $fieldrecord->type = 'gitlab';
+        $fieldrecord->configdata = '{}';
+
+        $field = new field_controller(0, $fieldrecord);
+        $datarecord = new \stdClass();
+        $datarecord->fieldid = 0;
+
+        $controller = new data_controller(0, $datarecord, $field);
+
+        $this->assertNull($controller->export_value());
+    }
+
+    /**
+     * Test instance_form_definition adds element to form
+     */
+    public function test_instance_form_definition_adds_element(): void {
+        $fieldrecord = new \stdClass();
+        $fieldrecord->name = 'GitLab Token';
+        $fieldrecord->shortname = 'gitlab_token';
+        $fieldrecord->type = 'gitlab';
+        $fieldrecord->configdata = json_encode(['required' => false]);
+
+        $field = new field_controller(0, $fieldrecord);
+        $datarecord = new \stdClass();
+        $datarecord->fieldid = 0;
+
+        $controller = new data_controller(0, $datarecord, $field);
+
+        $mform = $this->createMock(\MoodleQuickForm::class);
+        $mform->expects($this->once())->method('addElement');
+        $mform->expects($this->once())->method('setType');
+        $mform->expects($this->once())->method('setDefault');
+
+        $controller->instance_form_definition($mform);
+    }
+
+    /**
+     * Test instance_form_definition adds required rule when required is true
+     */
+    public function test_instance_form_definition_adds_required_rule(): void {
+        $fieldrecord = new \stdClass();
+        $fieldrecord->name = 'GitLab Token';
+        $fieldrecord->shortname = 'gitlab_token';
+        $fieldrecord->type = 'gitlab';
+        $fieldrecord->configdata = json_encode(['required' => true]);
+
+        $field = new field_controller(0, $fieldrecord);
+        $datarecord = new \stdClass();
+        $datarecord->fieldid = 0;
+
+        $controller = new data_controller(0, $datarecord, $field);
+
+        $mform = $this->createMock(\MoodleQuickForm::class);
+        $mform->expects($this->once())->method('addElement');
+        $mform->expects($this->once())->method('setType');
+        $mform->expects($this->once())->method('setDefault');
+        $mform->expects($this->once())->method('addRule');
+
+        $controller->instance_form_definition($mform);
+    }
+
+    /**
+     * Test get_value decrypts encrypted value
+     */
+    public function test_get_value_decrypts_value(): void {
+        $originalvalue = 'test_token_12345';
+        $encryptedvalue = encryption::encrypt($originalvalue);
+
+        $fieldrecord = new \stdClass();
+        $fieldrecord->name = 'GitLab Token';
+        $fieldrecord->shortname = 'gitlab_token';
+        $fieldrecord->type = 'gitlab';
+        $fieldrecord->configdata = '{}';
+
+        $field = new field_controller(0, $fieldrecord);
+        $datarecord = new \stdClass();
+        $datarecord->fieldid = 0;
+        $datarecord->value = $encryptedvalue;
+
+        $controller = new data_controller(0, $datarecord, $field);
+        $decryptedvalue = $controller->get_value();
+
+        $this->assertEquals($originalvalue, $decryptedvalue);
+    }
+
+    /**
+     * Test instance_form_save encrypts and saves value
+     */
+    public function test_instance_form_save_encrypts_value(): void {
+        $fieldrecord = new \stdClass();
+        $fieldrecord->name = 'GitLab Token';
+        $fieldrecord->shortname = 'gitlab_token';
+        $fieldrecord->type = 'gitlab';
+        $fieldrecord->configdata = json_encode(['required' => false]);
+
+        $field = new field_controller(0, $fieldrecord);
+        $datarecord = new \stdClass();
+        $datarecord->fieldid = 0;
+        $datarecord->id = 0;
+        $datarecord->value = '';
+
+        $controller = new data_controller(0, $datarecord, $field);
+
+        $formdata = new \stdClass();
+        $formdata->customfield_gitlab_token = 'my_secret_token';
+
+        // Mock the data and category
+        $controllermock = $this->getMockBuilder(data_controller::class)
+            ->setConstructorArgs([0, $datarecord, $field])
+            ->onlyMethods(['save', 'get_form_element_name'])
+            ->getMock();
+
+        $controllermock->expects($this->once())->method('get_form_element_name')
+            ->willReturn('customfield_gitlab_token');
+        $controllermock->expects($this->once())->method('save');
+
+        // This should not throw an exception
+        $controllermock->instance_form_save($formdata);
+    }
+
+    /**
+     * Test instance_form_save skips if element doesn't exist in form data
+     */
+    public function test_instance_form_save_skips_missing_element(): void {
+        $fieldrecord = new \stdClass();
+        $fieldrecord->name = 'GitLab Token';
+        $fieldrecord->shortname = 'gitlab_token';
+        $fieldrecord->type = 'gitlab';
+        $fieldrecord->configdata = json_encode(['required' => false]);
+
+        $field = new field_controller(0, $fieldrecord);
+        $datarecord = new \stdClass();
+        $datarecord->fieldid = 0;
+
+        $controller = new data_controller(0, $datarecord, $field);
+
+        $formdata = new \stdClass();
+        // Don't add the form element
+
+        // Should not throw an exception
+        $controller->instance_form_save($formdata);
+
+        $this->assertTrue(true);
+    }
+}
---
+++ plugins/module/tests/webhook_test.php
@@ -0,0 +1,284 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Tests for Webhook class
+ *
+ * @package     mod_gitlab
+ * @copyright   2026 Léonard Jouve leonard.jouve@gmail.com
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_gitlab\local;
+
+use advanced_testcase;
+use core\encryption;
+
+/**
+ * Unit tests for Webhook class
+ *
+ * @package     mod_gitlab
+ * @copyright   2026 Léonard Jouve leonard.jouve@gmail.com
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class webhook_test extends advanced_testcase {
+
+    public function setUp(): void {
+        parent::setUp();
+        $this->resetAfterTest(true);
+    }
+
+    /**
+     * Test generate_key creates a valid base64 encoded 32-byte key
+     */
+    public function test_generate_key_creates_valid_key(): void {
+        $key = Webhook::generate_key();
+
+        // Verify it's valid base64
+        $decoded = base64_decode($key, true);
+        $this->assertNotFalse($decoded);
+
+        // Verify it's 32 bytes
+        $this->assertEquals(32, strlen($decoded));
+    }
+
+    /**
+     * Test generate_key produces different values each time
+     */
+    public function test_generate_key_produces_different_values(): void {
+        $key1 = Webhook::generate_key();
+        $key2 = Webhook::generate_key();
+
+        $this->assertNotEquals($key1, $key2);
+    }
+
+    /**
+     * Test get_module_key returns null when module doesn't exist
+     */
+    public function test_get_module_key_returns_null_when_not_exists(): void {
+        $key = Webhook::get_module_key(99999);
+        $this->assertNull($key);
+    }
+
+    /**
+     * Test get_module_key returns null when webhook_secret is empty
+     */
+    public function test_get_module_key_returns_null_when_empty(): void {
+        global $DB;
+
+        $course = $this->getDataGenerator()->create_course();
+        $module = new \stdClass();
+        $module->course = $course->id;
+        $module->name = 'Test Module';
+        $module->intro = '';
+        $module->introformat = FORMAT_HTML;
+        $module->webhook_secret = '';
+        $moduleid = $DB->insert_record('gitlab', $module);
+
+        $key = Webhook::get_module_key($moduleid);
+        $this->assertNull($key);
+    }
+
+    /**
+     * Test get_module_key returns decrypted key when it exists
+     */
+    public function test_get_module_key_returns_decrypted_key(): void {
+        global $DB;
+
+        $course = $this->getDataGenerator()->create_course();
+
+        $secretkey = base64_encode(random_bytes(32));
+        $encrypted_secret = encryption::encrypt($secretkey);
+
+        $module = new \stdClass();
+        $module->course = $course->id;
+        $module->name = 'Test Module';
+        $module->intro = '';
+        $module->introformat = FORMAT_HTML;
+        $module->webhook_secret = $encrypted_secret;
+        $moduleid = $DB->insert_record('gitlab', $module);
+
+        $key = Webhook::get_module_key($moduleid);
+
+        $this->assertNotNull($key);
+        $this->assertEquals(32, strlen($key));
+    }
+
+    /**
+     * Test get_signature produces valid signature
+     */
+    public function test_get_signature_produces_valid_signature(): void {
+        $webhook_id = 'webhook-123';
+        $webhook_timestamp = 1234567890;
+        $body = '{"key": "value"}';
+        $key = base64_decode(Webhook::generate_key(), true);
+
+        $signature = Webhook::get_signature($webhook_id, $webhook_timestamp, $body, $key);
+
+        // Verify signature starts with v1,
+        $this->assertStringStartsWith('v1,', $signature);
+
+        // Verify signature can be base64 decoded
+        $parts = explode(',', $signature);
+        $this->assertEquals(2, count($parts));
+        $this->assertNotFalse(base64_decode($parts[1], true));
+    }
+
+    /**
+     * Test get_signature produces deterministic output
+     */
+    public function test_get_signature_deterministic(): void {
+        $webhook_id = 'webhook-123';
+        $webhook_timestamp = 1234567890;
+        $body = '{"key": "value"}';
+        $key = base64_decode(Webhook::generate_key(), true);
+
+        $signature1 = Webhook::get_signature($webhook_id, $webhook_timestamp, $body, $key);
+        $signature2 = Webhook::get_signature($webhook_id, $webhook_timestamp, $body, $key);
+
+        $this->assertEquals($signature1, $signature2);
+    }
+
+    /**
+     * Test get_signature produces different signatures for different inputs
+     */
+    public function test_get_signature_different_for_different_inputs(): void {
+        $webhook_id = 'webhook-123';
+        $webhook_timestamp = 1234567890;
+        $body = '{"key": "value"}';
+        $key = base64_decode(Webhook::generate_key(), true);
+
+        $signature1 = Webhook::get_signature($webhook_id, $webhook_timestamp, $body, $key);
+        $signature2 = Webhook::get_signature($webhook_id, $webhook_timestamp + 1, $body, $key);
+        $signature3 = Webhook::get_signature($webhook_id, $webhook_timestamp, $body . 'x', $key);
+
+        $this->assertNotEquals($signature1, $signature2);
+        $this->assertNotEquals($signature1, $signature3);
+    }
+
+    /**
+     * Test is_valid returns true for valid signature
+     */
+    public function test_is_valid_returns_true_for_valid_signature(): void {
+        $webhook_id = 'webhook-123';
+        $webhook_timestamp = time();
+        $body = '{"key": "value"}';
+        $key = base64_decode(Webhook::generate_key(), true);
+
+        $signature = Webhook::get_signature($webhook_id, $webhook_timestamp, $body, $key);
+
+        $result = Webhook::is_valid($webhook_id, $webhook_timestamp, $signature, $body, $key);
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Test is_valid returns false for invalid signature
+     */
+    public function test_is_valid_returns_false_for_invalid_signature(): void {
+        $webhook_id = 'webhook-123';
+        $webhook_timestamp = time();
+        $body = '{"key": "value"}';
+        $key = base64_decode(Webhook::generate_key(), true);
+
+        $signature = 'v1,invalid_signature_data';
+
+        $result = Webhook::is_valid($webhook_id, $webhook_timestamp, $signature, $body, $key);
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Test is_valid returns false for expired signature
+     */
+    public function test_is_valid_returns_false_for_expired_signature(): void {
+        $webhook_id = 'webhook-123';
+        $old_timestamp = time() - (6 * 60); // 6 minutes ago
+        $body = '{"key": "value"}';
+        $key = base64_decode(Webhook::generate_key(), true);
+
+        $signature = Webhook::get_signature($webhook_id, $old_timestamp, $body, $key);
+
+        $result = Webhook::is_valid($webhook_id, $old_timestamp, $signature, $body, $key);
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Test is_valid returns false when body is tampered
+     */
+    public function test_is_valid_returns_false_for_tampered_body(): void {
+        $webhook_id = 'webhook-123';
+        $webhook_timestamp = time();
+        $body = '{"key": "value"}';
+        $key = base64_decode(Webhook::generate_key(), true);
+
+        $signature = Webhook::get_signature($webhook_id, $webhook_timestamp, $body, $key);
+
+        $tampered_body = '{"key": "hacked"}';
+        $result = Webhook::is_valid($webhook_id, $webhook_timestamp, $signature, $tampered_body, $key);
+        $this->assertFalse($result);
+    }
+
+    /**
+     * Test is_valid accepts multiple space-separated signatures
+     */
+    public function test_is_valid_accepts_multiple_signatures(): void {
+        $webhook_id = 'webhook-123';
+        $webhook_timestamp = time();
+        $body = '{"key": "value"}';
+        $key = base64_decode(Webhook::generate_key(), true);
+
+        $valid_signature = Webhook::get_signature($webhook_id, $webhook_timestamp, $body, $key);
+        $invalid_signature = 'v1,invalid';
+
+        $multiple_signatures = $invalid_signature . ' ' . $valid_signature;
+
+        $result = Webhook::is_valid($webhook_id, $webhook_timestamp, $multiple_signatures, $body, $key);
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Test get_content returns decoded JSON object
+     */
+    public function test_get_content_returns_decoded_json(): void {
+        $json_body = '{"action": "push", "project": {"id": 123, "name": "test"}}';
+
+        $result = Webhook::get_content($json_body);
+
+        $this->assertIsObject($result);
+        $this->assertEquals('push', $result->action);
+        $this->assertEquals(123, $result->project->id);
+        $this->assertEquals('test', $result->project->name);
+    }
+
+    /**
+     * Test get_content returns null for invalid JSON
+     */
+    public function test_get_content_returns_null_for_invalid_json(): void {
+        $invalid_json = 'not valid json {]';
+
+        $result = Webhook::get_content($invalid_json);
+
+        $this->assertNull($result);
+    }
+
+    /**
+     * Test get_content returns null for empty string
+     */
+    public function test_get_content_returns_null_for_empty_string(): void {
+        $result = Webhook::get_content('');
+
+        $this->assertNull($result);
+    }
+}
